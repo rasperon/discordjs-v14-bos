4717650cbe97f3c0072b49e3f603a3bc1617da59
Rasperon C <143193225+rasperon@users.noreply.github.com>
2024-04-15 08:29:53 +0400

    Create rasperon.js

---
+++ rasperon.js
@@ -0,0 +1,59 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits, ActivityType } = require('discord.js');
+const { token, mongoUrl } = require('./config.json');
+const mongoose = require('mongoose');
+
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMembers,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent
+] });
+
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[UYARI]  ${filePath} kısmında "data" veya "execute" eksik.`);
+		}
+	}
+}
+
+/*                                      Status ayarlama                            */
+client.on('ready', (c) => {
+    client.user.setActivity({
+        name: 'Developed by 💝 Rasperon',
+        type: ActivityType.Custom, //playing, listening vb.
+    })
+})
+
+/*                                     EVENT AKTARIMI                                                 */
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+(async () => {
+	await mongoose.connect(mongoUrl);
+	console.log("[+] Veritabanı ile bağlantı kuruldu.");
+	
+	client.login(token);
+})();
